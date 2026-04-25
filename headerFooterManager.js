4332b6961986a3d3523d21708f55b22a4474750d
Leon Benjamignan <leon.benjamignan@hey.com>
2026-04-24 18:04:35 -0700

    updated NOW page

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -4,6 +4,8 @@ class NavHeader extends HTMLElement {
         <h1><a href='/'>Leon Benjamignan</a></h1>
         <nav class="navbar">
         <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="now.html">Now</a></li>
             <li><a href="experience.html">Experience</a></li>
             <li><a href="https://world.hey.com/leon.benjamignan">Blog</a></li>
             <li><a href="speaking.html">Speaking</a></li>
@@ -21,14 +23,12 @@ class CtaFooter extends HTMLElement {
 <footer>
 <hr>
 <p>© 2026 <a href="/">Leon Benjamignan</a> — 
-   <a href="https://world.hey.com/leon.benjamignan">Read my blog</a> • 
-   <a href="experience.html">Work History</a>
+   <a href="https://world.hey.com/leon.benjamignan">Read my blog</a>
 </p>
 </footer>
 `;
     }
 }
 
-
-customElements.define('nav-header',NavHeader)
-customElements.define('cta-footer',CtaFooter)
+customElements.define('nav-header', NavHeader);
+customElements.define('cta-footer', CtaFooter);
